fd3b4b4c849189cedee0715f1527bac5d26602d1
phrawzty <phrawzty+github@gmail.com>
2012-11-16 17:20:14 +0100

    initial POC commit

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-raas
+RaaS
 ====
 
-Random as a service!
+This is a simple Sinatra application for generating and providing strings full
+of random characters via HTTP.
+
+What?
+-----
+
+Yeah, we know.  Deal with it.
+
+Disclaimer
+----------
+
+This is a *proof of concept* and no guarantee is made to the fitness (or,
+indeed, usefulness) of this application whatsoever.  Please enjoy responsibly.
+
+Requirements
+------------
+
+ * Ruby v1.9.3 or better; anything less would be uncivilised.
+ * The `sinatra` and `xml-simple` gems, along with other standard modules.
+
+Usage
+-----
+
+So the API can deal with this : `/:enc/:charset/:len/:num`
+
+ * Valid `:enc` are `p`, `j`, `y`, `h`, and `x`.
+ * Valid `:charset` are `ab`, `as`, `hex`, and `num`.
+ * `:len` is length of each result.
+ * `:num` is the number of results.
+
+Run it like this : `$ ruby ./raas.rb`
+
+License
+-------
+
+Released under the ASL 2.0; see `LICENSE` for details.
---
+++ raas.rb
@@ -0,0 +1,100 @@
+# This will dump some debug output to console and enable classy Sinatra errors.
+@debug = true
+
+require 'securerandom' 
+require 'sinatra'
+require 'yaml'
+require 'json'
+require 'xmlsimple'
+if ( @debug == true ) then
+    require 'pp'
+end
+
+
+# Determine the character set to use.
+def charset(type = nil)
+    charset = ''
+
+    puts "DEBUG: charset: type => #{type}" unless @debug == false
+
+    charset = case type
+        when 'ab' then '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        when 'as' then '23456789abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ'
+        when 'hex' then '0123456789abcdef'
+        when 'num' then '0123456789'
+        else raise 'invalid charset'
+    end
+
+    return charset
+end
+
+# Generate the string.
+def generator(charset = false, len = 16)
+    result = ''
+
+    puts "DEBUG: generator: charset => #{charset}, len => #{len}" unless @debug == false
+
+    # Some sort of sanity checking. :P
+    raise 'charset must be defined' unless ! charset == false
+
+    # Unleash the fury !
+    SecureRandom.random_bytes(len.to_i).unpack("C*").each { |a| result += charset[a%charset.length] }
+
+    return result
+end
+
+# The result encodatron.
+def encoder(enc = false, result = nil)
+    puts "DEBUG: encoder: enc => #{enc}, result => #{result}" unless @debug == false
+
+    # Some sort of sanity checking. :P
+    raise 'encoding must be defined' unless ! enc == false
+    raise 'result must be an array' unless result.is_a?(Array)
+
+    type = nil
+    type = case enc
+        when 'p' then 'plain'
+        when 'y' then 'yaml'
+        when 'j' then 'json'
+        when 'h' then 'html'
+        when 'x' then 'xml'
+        else raise 'encoding is something strange and terrifying'
+    end
+
+    puts "DEBUG: encoder: type => #{type}" unless @debug == false
+
+    erb "result_#{type}".to_sym, :content_type => "text/#{type}", :locals => { :result => result }
+end
+
+# Routes go here.
+def routes
+	get '/' do
+		erb :index
+	end
+
+    # The basic GET API, heh.
+    get '/:enc/:charset/:len/:num' do
+        pp params unless @debug == false
+
+        # Determine the character set to use.
+        set = charset(params[:charset])
+
+        # Build an array full of random.
+        result = []
+        params[:num].to_i.times do
+            result.push(generator(set, params[:len]))
+        end
+
+        # Give the array back the way the user wants it.
+        encoder(params[:enc], result)
+    end
+
+    get '*' do
+        erb :dunno
+    end
+end
+
+
+# Houston, we are go for runtime.
+set :show_exceptions, false unless @debug == true
+routes
